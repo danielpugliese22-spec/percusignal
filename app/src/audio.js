90e13c3539369a4a9c15a495a8bda64ed5a29f5c
danielpugliese22-spec <danielpugliese22@gmail.com>
2026-06-03 10:56:43 -0300

    modificacion fetch audio.js

--- app/src/audio.js
+++ app/src/audio.js
@@ -134,7 +134,7 @@ const _sampleBuffers = {};
 async function loadSampleBuffer(instrId) {
   if(_sampleBuffers[instrId]) return _sampleBuffers[instrId];
   initAudio();
-  const res = await fetch(`/app/audio/${instrId}.wav`);
+  const res = await fetch(`/assets/audio/${instrId}.wav`);
   const arr = await res.arrayBuffer();
   _sampleBuffers[instrId] = await state.audioCtx.decodeAudioData(arr);
   return _sampleBuffers[instrId];
